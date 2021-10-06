974964b00e40f8cbc35d785eddb84c536402376d
Apvictor <armando2019ti@gmail.com>
2021-10-05 22:41:10 -0300

    Update api

--- DoctorOn_BackEnd/routes/api.php
+++ DoctorOn_BackEnd/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UnitsController;
 use App\Http\Controllers\UserController;
 
 Route::post('register', [RegisterController::class, 'register']);
@@ -11,7 +12,16 @@ Route::post('login', [LoginController::class, 'authenticate']);
 Route::group(['middleware' => ['jwt.verify']], function () {
     Route::get('logout', [LoginController::class, 'logout']);
     Route::get('profile', [UserController::class, 'profile']);
-    
+
+
+
+    Route::post('users/update/{user}',  [UserController::class, 'update']);
+
+
+    Route::get('units', [UnitsController::class, 'index']);
+
+
+
     Route::get('products', [ProductController::class, 'index']);
     Route::get('products/show/{id}', [ProductController::class, 'show']);
     Route::post('products/store', [ProductController::class, 'store']);
