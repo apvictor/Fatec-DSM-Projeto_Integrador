3924f2c01002f6d22aca5c64f2716bd7330a3ac7
Armando Víctor Pereira <armandinho14.ap@gmail.com>
2021-10-22 20:19:12 -0300

    Updates

---
+++ src/app/units/details-units/details-units.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-units',
+  templateUrl: './details-units.page.html',
+  styleUrls: ['./details-units.page.scss'],
+})
+export class DetailsUnitsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
