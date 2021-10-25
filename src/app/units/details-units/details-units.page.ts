b63317cacff808290b7aa72a7ae69cc187e400ad
Armando Víctor Pereira <armandinho14.ap@gmail.com>
2021-10-25 14:45:17 -0300

    Exibir mapa e traçar rotas

--- src/app/units/details-units/details-units.page.ts
+++ src/app/units/details-units/details-units.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-details-units',
@@ -7,9 +11,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsUnitsPage implements OnInit {
 
-  constructor() { }
+  public id: any;
+  public unidade: any = [];
 
-  ngOnInit() {
+  map: google.maps.Map;
+  minhaPosicao: google.maps.LatLng;
+
+  private direction = new google.maps.DirectionsService();
+  private directionsRender = new google.maps.DirectionsRenderer();
+
+  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private authService: AuthService,
+    public http: HttpClient,
+    private geolacation: Geolocation,
+  ) {
+  }
+
+  async ngOnInit() {
+    this.id = this.activateRoute.snapshot.paramMap.get('id');
+    this.authService.unitsDetails(this.id).subscribe(
+      (units) => {
+        this.unidade = units;
+        this.exibirMapa();
+        this.tracarRota(this.unidade.unit);
+      }
+    );
+  }
+
+  exibirMapa() {
+    this.buscarMinhaPosicao();
+    const opcoes = {
+      center: this.minhaPosicao,
+      zoom: 15,
+      disableDefaultUI: true
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
+  }
+
+  async buscarMinhaPosicao() {
+    this.geolacation.getCurrentPosition()
+      .then((resp) => {
+        this.minhaPosicao = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+        this.irParaMinhaPosicao();
+      })
+      .catch((error) => {
+        console.log('Erro', error);
+      })
+  }
+
+  irParaMinhaPosicao() {
+    this.map.setCenter(this.minhaPosicao);
+    this.map.setZoom(15);
+  }
+
+
+  public tracarRota(unidadePosicao) {
+
+    new google.maps.Geocoder().
+      geocode({ address: unidadePosicao },
+        resultado => {
+          this.map.setCenter(resultado[0].geometry.location);
+
+          const rota: google.maps.DirectionsRequest = {
+            origin: this.minhaPosicao,
+            destination: unidadePosicao,
+            unitSystem: google.maps.UnitSystem.METRIC,
+            travelMode: google.maps.TravelMode.DRIVING
+          }
+
+          this.direction.route(rota, (result, status) => {
+            if (status == 'OK') {
+              this.directionsRender.setMap(this.map);
+              this.directionsRender.setDirections(result);
+            }
+          });
+
+        });
   }
 
 }
